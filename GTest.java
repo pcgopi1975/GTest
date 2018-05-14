d216f97ff04668a96bde5c039f6e4eabc38fc330
Gopi <pcgopi@gmail.com>
2018-05-14 18:34:02 -0400

    Master to Dev checkin

--- GTest.java
+++ GTest.java
@@ -2,6 +2,7 @@ public class GTest {
 
     public static void main(String[] args){
         System.out.println("Gtest");
-        System.out.println("Gtest dev1 checkin update");
+        System.out.println("Gtest master checkin updates");
+        System.out.println("Gtest master check to dev checkin updates");
     }
 }
